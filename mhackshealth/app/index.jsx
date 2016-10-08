ce1bc3a3f63a26f57aed905c911ca5dfd331a4ad
Sugata <sugata.a@gmail.com>
2016-10-08 16:42:40 -0400

    refactor and test

--- mhackshealth/app/index.jsx
+++ mhackshealth/app/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Notes from './components/Notes'
+
 if(process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf');
 }
